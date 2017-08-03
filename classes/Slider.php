791ce9cf9291bbab695d9fdb8a12b3a80d641ec5
Alexander Hamilton <alexanderhamilton@As-MacBook-Pro-2.local>
2017-08-02 21:35:31 -0400

    [fixed] added style the old (non-working) way.

--- classes/Slider.php
+++ classes/Slider.php
@@ -6,7 +6,7 @@ class Slider {
 
 	public static function make( $file, $data, $identifier )
 	{
-		echo Style::$file( $identifier );
+		ReusableClasses::addfile( "slider", $file );
 		$View = View::factory( 'reusables/views/slider/' . $file );
 		$View->set( 'sliderdict', $data );
 		$View->set( 'identifier', $identifier );
