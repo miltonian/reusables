b2720a33f07653f825d85f912a02a78b805f9adb
Alexander Hamilton <alexanderhamilton@As-MacBook-Pro-2.local>
2017-10-02 20:17:18 -0400

    fixes to cellactions

--- views/cell/imagetext_inline_edit.php
+++ views/cell/imagetext_inline_edit.php
@@ -35,7 +35,7 @@ $viewoptions = ReusableClasses::convertViewActions( $viewoptions );
 			}else if( $actiontype == "dropdown" ){
 				$cellactionshtml .= "<div class='dropdown'>";
 					$cellactionshtml .= "<div class='inner-dropdown'>";
-						$cellactionshtml .= "<button id='" . Data::getValue( $viewdict, 'index' ) . "' class='inner-dropbtn imagetext_inline_edit action actionindex_" . $i . " index_" . $cellindex . "' style='background-image: url(" . $action['backgroundimage'] . ");'></button>";
+						$cellactionshtml .= "<button class='inner-dropbtn imagetext_inline_edit action actionindex_" . $i . " index_" . $cellindex . "' style='background-image: url(" . $action['backgroundimage'] . ");'></button>";
 						$cellactionshtml .= "<div id='inner-myDropdown_" . Data::getValue( $viewdict, 'index' ) . "' class='inner-dropdown-content'>";
 							for ($a=0; $a < sizeof( $action['dropdown_array'] ); $a++) {
 								$dropdownarray = $action['dropdown_array'];
@@ -44,6 +44,9 @@ $viewoptions = ReusableClasses::convertViewActions( $viewoptions );
 						$cellactionshtml .= "</div>";
 					$cellactionshtml .= "</div>";
 				$cellactionshtml .= "</div>";
+
+				// Data::addData( ["list" => $action['dropdown_array']], $identifier . "dropdown" );
+				// Menu::make( "dropdown", $identifier . "dropdown" );
 			}else{
 				$cellactionshtml .= "<button id='" . Data::getValue( $viewdict, 'index' ) . "' class='imagetext_inline_edit action actionindex_" . $i . " index_" . $cellindex . "' style='background-image: url(" . $action['backgroundimage'] . ");'></button>";
 			}
@@ -98,20 +101,31 @@ $viewoptions = ReusableClasses::convertViewActions( $viewoptions );
 
 	function imagetext_inline_edit_start() {
 		imagetext_inline_edit.setupactions( cellactions, editingfunctions, viewdict );
-		
+
 		$('.inner-dropbtn.imagetext_inline_edit.action').click(function(){
-			var actionindex = Reusable.getIndexFromClass( "actionindex_")
+
+			var cellindex = Reusable.getIndexFromClass( "index_", this)
 			// CHECK AGAIN
-			document.getElementById("inner-myDropdown_"+actionindex).classList.toggle("show");
+			var dropdowns = document.getElementsByClassName("inner-dropdown-content");
+			var i;
+			for (i = 0; i < dropdowns.length; i++) {
+				var openDropdown = dropdowns[i];
+				if (openDropdown.classList.contains('show')) {
+					openDropdown.classList.remove('show');
+				}
+			}
+			document.getElementById("inner-myDropdown_"+cellindex).classList.toggle("show");
 		});
 	}
 
 	function dropdownaction(e) {
 		// e.preventDefault();
+
 		document.getElementById("inner-myDropdown").classList.toggle("show");
 	}
 
-	window.onclick = function(e) {
+	// window.onclick = function(e) {
+	$(window).click(function(e){
 		// e.preventDefault();
 		if (!e.target.matches('.inner-dropbtn')) {
 
@@ -126,5 +140,5 @@ $viewoptions = ReusableClasses::convertViewActions( $viewoptions );
 		}else{
 			e.preventDefault();
 		}
-	}
+	})
 </script>
