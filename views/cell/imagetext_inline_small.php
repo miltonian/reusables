8826c6543c7670bef20babc6e65cdfebf7444737
Alexander Hamilton <alexanderhamilton@As-MacBook-Pro-2.local>
2017-10-09 23:48:51 -0400

    forgot to add these changes

--- views/cell/imagetext_inline_small.php
+++ views/cell/imagetext_inline_small.php
@@ -12,32 +12,7 @@ namespace Reusables;
 		]
 	*/
 	
-		$data_id = Data::getDefaultDataID( $viewdict );
-	$fullviewdict = Data::getFullArray( $viewdict );
-	if(!isset($viewdict)){ $cell7mediatype=""; }
-	if( !isset($viewdict['type'])){ $viewdict['type'] = ""; }
-	if( !isset($viewdict['isfeatured']) ){ $viewdict['isfeatured']=false; }
-	// if( !isset($mediatype) ){ $mediatype="post"; }
-	if( !isset($isadmin) ){ $isadmin=false; }
-	// if(!isset($viewdict['id'])){ $viewdict['id'] = $viewdict['id']; }
-
-	$linkpath = "";
-	$linkpath .= Data::getValue( $viewdict, 'pre_slug' );
-	$linkpath .= Data::getValue( $viewdict, 'slug' );
-
-	$mediatype = Data::getValue( $viewdict, 'mediatype' );
-
-	// echo Data::getValue( $viewdict, 'id' )
-	$cellindex = Data::getValue( $viewdict, 'index' );
-	// echo json_encode($cellindex);
-	// exit(json_encode($cellindex));
-
-	$description = implode(' ', array_slice( explode(' ', strip_tags(Data::getValue( $viewdict, 'html_text' ))), 0, 10) ) . "...";
-	if( $description == "..." ){
-		$description = "";
-	}
-
-	$celldate = Data::getValue( $viewdict, 'date' );
+	extract( Cell::prepareCell( $identifier ) );
 
 ?>
 
--- views/section/imageleft_gridright.php
+++ views/section/imageleft_gridright.php
@@ -36,14 +36,17 @@ foreach ($images as $im) {
 	}
 	$linkpath = Data::getValue( $viewoptions, 'pre_slug' ) . Data::getValue( $viewdict, 'slug' );
 }
-// exit( json_encode( $images ) );
+
+$fullviewdict = Data::getFullArray( $viewdict )[$identifier]['value'];
+$optiontype = Data::getValue( $viewoptions, 'type' );
+// exit( json_encode( $fullviewdict ) );
 
 ?>
 
 
 <div class="viewtype_section imageleft_gridright main <?php echo $identifier ?>">
 	<div class="imageleft_gridright left post" style="background-image: url('<?php echo Data::getValue( $images[0], 'featured_imagepath' ) ?>'">
-		<a class="imageleft_gridright link one" href="<?php echo Data::getValue( $viewoptions, 'pre_slug' ) ?><?php echo Data::getValue( $images[0], 'slug' ) ?>">
+		<a class="imageleft_gridright link one index_1" href="<?php echo Data::getValue( $viewoptions, 'pre_slug' ) ?><?php echo Data::getValue( $images[0], 'slug' ) ?>">
 			<div class="imageleft_gridright gradient">
 				<h1 class="imageleft_gridright title"><?php echo Data::getValue( $images[0], 'title' ) ?></h1>
 			</div>
@@ -52,14 +55,14 @@ foreach ($images as $im) {
 	<div class="imageleft_gridright right">
 		<div class="imageleft_gridright top">
 			<div class="imageleft_gridright topleft post" style="background-image: url('<?php echo Data::getValue( $images[1], 'featured_imagepath' ) ?>'">
-				<a class="imageleft_gridright link two" href="<?php echo Data::getValue( $viewoptions, 'pre_slug' ) ?><?php echo Data::getValue( $images[1], 'slug' ) ?>">
+				<a class="imageleft_gridright link two index_2" href="<?php echo Data::getValue( $viewoptions, 'pre_slug' ) ?><?php echo Data::getValue( $images[1], 'slug' ) ?>">
 					<div class="imageleft_gridright gradient">
 						<h1 class="imageleft_gridright title"><?php echo Data::getValue( $images[1], 'title' ) ?></h1>
 					</div>
 				</a>
 			</div>
 			<div class="imageleft_gridright topright post" style="background-image: url('<?php echo Data::getValue( $images[2], 'featured_imagepath' ) ?>')">
-				<a class="imageleft_gridright link three" href="<?php echo Data::getValue( $viewoptions, 'pre_slug' ) ?><?php echo Data::getValue( $images[2], 'slug' ) ?>">
+				<a class="imageleft_gridright link three index_3" href="<?php echo Data::getValue( $viewoptions, 'pre_slug' ) ?><?php echo Data::getValue( $images[2], 'slug' ) ?>">
 					<div class="imageleft_gridright gradient">
 						<h1 class="imageleft_gridright title"><?php echo Data::getValue( $images[2], 'title' ) ?></h1>
 					</div>
@@ -67,7 +70,7 @@ foreach ($images as $im) {
 			</div>
 		</div>
 		<div class="imageleft_gridright bottom post" style="background-image: url('<?php echo Data::getValue( $images[3], 'featured_imagepath' ) ?>'">
-			<a class="imageleft_gridright link four" href="<?php echo Data::getValue( $viewoptions, 'pre_slug' ) ?><?php echo Data::getValue( $images[3], 'slug' ) ?>">
+			<a class="imageleft_gridright link four index_4" href="<?php echo Data::getValue( $viewoptions, 'pre_slug' ) ?><?php echo Data::getValue( $images[3], 'slug' ) ?>">
 				<div class="imageleft_gridright gradient">
 					<h1 class="imageleft_gridright title"><?php echo Data::getValue( $images[3], 'title' ) ?></h1>
 				</div>
@@ -75,3 +78,48 @@ foreach ($images as $im) {
 		</div>
 	</div>
 </div>
+
+<script>
+	
+	var viewdict = <?php echo json_encode( $viewdict ) ?>;
+	var viewoptions = <?php echo json_encode( $viewoptions ) ?>;
+
+	var thismodalclass = "";
+	<?php $celltype = "" ?>
+	var type = <?php echo json_encode( $optiontype ) ?>;
+
+	<?php if( $celltype == "modal" ){ ?>
+		thismodalclass = new <?php echo $viewoptions['modal']['modalclass'] ?>Classes();
+		var dataarray = <?php echo json_encode( $fullviewdict ) ?>;
+	<?php } ?>
+
+	var viewdict = <?php echo json_encode($viewdict) ?>;
+	var viewoptions = <?php echo json_encode( $viewoptions ) ?>;
+
+	$('.<?php echo $identifier ?> .imageleft_gridright.link').off().click(function(e){ 
+		<?php $arrayindex = 1; ?>
+
+		var optiontype = <?php echo json_encode($optiontype) ?>;
+		if( optiontype == "modal" || optiontype == "dropdown" ) { 
+			e.preventDefault();
+			if( typeof dataarray === "undefined" ) { 
+				dataarray = []
+			}
+			Reusable.addAction( viewdict, [thismodalclass], 0, dataarray, this, e, viewoptions );
+		}
+
+		<?php 
+			ReusableClasses::getEditingFunctionsJS( $viewoptions ) ;
+		?>
+
+		if( typeof dataarray === "undefined" ) {
+			dataarray = []
+		}
+		var viewdict = <?php echo json_encode($viewdict) ?>;
+		var viewoptions = <?php echo  json_encode( $viewoptions ) ?>;
+		Reusable.addAction( viewdict, [thismodalclass], 0, dataarray, this, e, viewoptions );
+
+
+	});
+
+</script>
