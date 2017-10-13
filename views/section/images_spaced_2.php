1026138dc6c866ef0b541687c736270d3eb8848f
Alexander Hamilton <alexanderhamilton@As-MacBook-Pro-2.local>
2017-10-12 20:54:10 -0400

    added links to images_spaced_2

--- views/section/images_spaced_2.php
+++ views/section/images_spaced_2.php
@@ -11,6 +11,9 @@ namespace Reusables;
 
 		// exit( json_encode( Data::getValue($viewdict['left_imagepath']) ) );
 
+		$left_linkpath = Data::getValue( $viewdict, "left_link" );
+		$right_linkpath = Data::getValue( $viewdict, "right_link" );
+
 ?>
 
 <style>
@@ -18,8 +21,20 @@ namespace Reusables;
 
 <div class="viewtype_section <?php echo $identifier ?> section_8 main">
 	<div class="section_8 wrapper">
+		<?php if( $left_linkpath != "" ) { ?>
+			<a href="<?php echo $left_linkpath ?>" >
+		<?php } ?>
 		<div class="section_8 left" style="background-image: url('<?php echo Data::getValue($viewdict, 'left_imagepath') ?>');"></div>
+		<?php if( $left_linkpath != "" ) { ?>
+			</a>
+		<?php } ?>
+		<?php if( $right_linkpath != "" ) { ?>
+			<a href="<?php echo $right_linkpath ?>">
+		<?php } ?>
 		<div class="section_8 right" style="background-image: url('<?php echo Data::getValue($viewdict, 'right_imagepath') ?>');"></div>
+		<?php if( $right_linkpath != "" ) { ?>
+			</a>
+		<?php } ?>
 	</div>
 </div>
 
