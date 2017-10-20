98e988c874e3251c0a2cfa2b0f5b71feed7f7c92
Alexander Hamilton <alexanderhamilton@As-MacBook-Pro-2.local>
2017-10-19 23:38:19 -0400

    added views: title_withsubtitles and request_form

---
+++ views/header/title_withsubtitles.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Reusables;
+
+$subtitles = Data::getValue( $viewdict, 'subtitles' );
+if( $subtitles == "" ) { $subtitles = []; }
+
+?>
+
+<style>
+	
+</style>
+
+<div class="viewtype_header title_withsubtitles main <?php echo $identifier ?>">
+	<div class="title_withsubtitles title_container">
+		<h3 class="title_withsubtitles title"><?php echo Data::getValue( $viewdict, "title" ) ?></h3>
+	</div>
+
+	<div class="title_withsubtitles subtitles_div">
+		<?php $i=0; ?>
+		<?php foreach ($subtitles as $s) { ?>
+			<label class="title_withsubtitles subtitle index_<?php echo $i ?>"><?php echo $s ?></label>
+			<?php $i++; ?>
+		<?php } ?>
+
+	</div>
+</div>
