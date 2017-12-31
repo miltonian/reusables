1687a7d889bde92a28af830f0dfcd299129e071d
miltonian <miltonian3@gmail.com>
2017-12-31 16:45:58 -0500

    changed Ad Class

--- classes/Ad.php
+++ classes/Ad.php
@@ -4,6 +4,8 @@ namespace Reusables;
 
 class Ad {
 
+	protected static $ads_onpage = [];
+
 	public static function set( $file, $identifier )
 	{
 		Views::setDefaultViewInfo( $file, $identifier, "ad" );
@@ -12,6 +14,59 @@ class Ad {
 	public static function make( $file, $identifier )
 	{
 		return Views::makeView( $file, $identifier, "ad" );
+	}
+
+	public static function getAdForSet( $size_needed, $ads, $position=null )
+	{
+		if( isset( $ads['value'] ) ) {
+			$ads = Data::getValue( $ads )['value'];
+		}
+		shuffle($ads);
+		foreach ($ads as $a) {
+
+			if( isset( self::$ads_onpage[$a['id']] ) ) {
+				continue;
+			}
+
+			$adsize = "";
+			if( isset( $a['adsize'] ) ) {
+				$adsize = $a['adsize'];
+			}else if( isset( $a['size'] ) ) {
+				$adsize = $a['size'];
+			}else{
+				exit("needs adsize or size key in ad (/classes/Ad.php)");
+			}
+
+			if( $size_needed == $adsize ) {
+				if( $position != null ) {
+					if( $a['position'] != $position) {
+						continue;
+					}
+				}
+				$adsonpage_ids = array_keys(self::$ads_onpage);
+				$matches_partner = false;
+				foreach ($adsonpage_ids as $ad_dict) {
+					if( $ad_dict['core_partner'] == $a['core_partner'] ) {
+						$matches_partner = true;
+						break;
+					}
+				}
+				if( $matches_partner ) {
+					continue;
+				}
+				if( isset( $a['link_path'] ) && !isset( $a['link'] ) ) {
+					$a['link'] = $a['link_path'];
+				}
+				self::$ads_onpage[$a['id']] = $a;
+				return $a;
+
+			}
+
+		}
+
+
+
+		
 	}
 
 
