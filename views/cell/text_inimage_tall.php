fd6ad27de48c7908fb006fff72fc3048d070e39f
Alex <miltonian3@gmail.com>
2018-02-27 21:22:09 -0500

    added 2 views: text_inimage_tall and gallery_preview

---
+++ views/cell/text_inimage_tall.php
@@ -0,0 +1,58 @@
+<?php 
+
+	namespace Reusables;
+
+	$viewdict = Data::convertKeysInTable( $identifier, $viewdict );
+	
+	
+	extract( Cell::prepareCell( $identifier ) );
+
+?>
+
+<style>
+</style>
+
+
+
+<div class="viewtype_cell text_inimage_tall main <?php echo $identifier ?> index_<?php echo $cellindex ?> <?php if($mediatype=="youtube" || $mediatype=="podcast"){ echo $mediatype; } ?> index_<?php echo $cellindex ?>" >
+	<div class="text_inimage_tall container">
+		<div style="display: inline-block; width: 100%;">
+			<div>
+				<a href="<?php echo $linkpath ?>">
+					<div class="text_inimage_tall picture" style="<?php echo 'background-image: url('.Data::getValue( $viewdict, 'imagepath', $table_identifier ).');'; ?>; <?php if( $mediatype == "video" ){ echo 'padding-bottom: 0;';  } ?>)">
+						<?php if($mediatype == "video"){ ?>
+							<video width="100%" height="auto" autoplay loop>
+							  <source src="<?php echo Data::getValue( $viewdict, 'featured_imagepath', $table_identifier ) ?>" type="video/mp4">
+							  <source src="<?php echo Data::getValue( $viewdict, 'featured_imagepath', $table_identifier ) ?>" type="video/ogg">
+							Your browser does not support the video tag.
+							</video>
+						<?php } ?>
+						<div class="text_inimage_tall words">
+							<div class="text_inimage_tall gradient"></div>
+							<div class="text_inimage_tall text-container">
+								<!-- <label class="grey-label">Today</label> -->
+								<label class="text_inimage_tall category"><?php echo Data::getValue( $viewdict, 'category', $table_identifier ) ?></label>
+								<br>
+								<a href="<?php echo $linkpath ?>">
+									<label class="text_inimage_tall title" style=""><?php echo Data::getValue( $viewdict, 'title', $table_identifier ); ?></label>
+
+								</a>
+								<br>
+								<label class="text_inimage_tall grey-label"><?php echo $description ?></label>
+							</div>
+						</div>
+					</div>
+				</a>
+				
+			</div>
+		</div>
+	</div>
+</div>
+
+<script>
+
+	<?php
+		ReusableClasses::addEditingToCell( $identifier, $fullviewdict, $celltype );
+	?>;
+	
+</script>
---
+++ views/section/gallery_preview.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace Reusables;
+
+
+if( isset($viewdict['value']) ){
+	$images = $viewdict['value'];
+}else{
+	$images = $viewdict;
+}
+$i=0;
+foreach ($images as $im) {
+	if( isset($viewdict['value']) ){
+		$image = Data::formatCellWithDefaultData( $identifier , $i );
+	}else{
+		$image = Data::getValue( $images, $i );
+	}
+	$images[$i] = $image;
+	$i++;
+}
+
+
+foreach ($images as $im) {
+	if( isset( $viewoptions['pre_slug'] ) ) {
+		$preslug = Data::getValue( $viewoptions, 'pre_slug' );
+	}
+	$linkpath = Data::getValue( $viewoptions, 'pre_slug' ) . Data::getValue( $im, 'slug' );
+}
+
+$fullarray = Data::getFullArray( $viewdict );
+if( isset( $viewdict[$identifier]['value'] ) ) {
+	$fullviewdict = Data::getFullArray( $viewdict )[$identifier]['value'];
+}else{
+	$fullviewdict = $viewdict;
+}
+
+$optiontype = Data::getValue( $viewoptions, 'type' );
+
+?>
+
+
+<div class="viewtype_section gallery_preview main <?php echo $identifier ?>">
+
+	<div class="gallery_preview title_container">
+		<div class="gallery_preview line"></div>
+		<label class="gallery_preview title"><?php echo Data::getValue( $viewoptions, 'title' ) ?></label>
+	</div>
+
+	<a class="gallery_preview clicktoedit image one index_0" href="<?php echo Data::getValue( $viewoptions, 'pre_slug' ) ?><?php echo Data::getValue( $images[0], 'slug' ) ?>" style="background-image: url(<?php echo Data::getValue($images[0], 'imagepath') ?>)">
+	</a>
+	<a class="gallery_preview clicktoedit image two index_1" href="<?php echo Data::getValue( $viewoptions, 'pre_slug' ) ?><?php echo Data::getValue( $images[1], 'slug' ) ?>" style="background-image: url(<?php echo Data::getValue($images[1], 'imagepath') ?>)">
+	</a>
+	<a class="gallery_preview clicktoedit image three index_2" href="<?php echo Data::getValue( $viewoptions, 'pre_slug' ) ?><?php echo Data::getValue( $images[2], 'slug' ) ?>" style="background-image: url(<?php echo Data::getValue($images[2], 'imagepath') ?>)">
+	</a>
+
+	<a class="gallery_preview clicktoedit image four index_3" href="<?php echo Data::getValue( $viewoptions, 'pre_slug' ) ?><?php echo Data::getValue( $images[3], 'slug' ) ?>" style="background-image: url(<?php echo Data::getValue($images[3], 'imagepath') ?>)">
+	</a>
+	<a class="gallery_preview clicktoedit image five index_4" href="<?php echo Data::getValue( $viewoptions, 'pre_slug' ) ?><?php echo Data::getValue( $images[4], 'slug' ) ?>" style="background-image: url(<?php echo Data::getValue($images[4], 'imagepath') ?>)">
+	</a>
+	<a class="gallery_preview clicktoedit image six index_5" href="<?php echo Data::getValue( $viewoptions, 'pre_slug' ) ?><?php echo Data::getValue( $images[5], 'slug' ) ?>" style="background-image: url(<?php echo Data::getValue($images[5], 'imagepath') ?>)">
+	</a>
+
+	<a class="gallery_preview clicktoedit image seven index_6" href="<?php echo Data::getValue( $viewoptions, 'pre_slug' ) ?><?php echo Data::getValue( $images[6], 'slug' ) ?>" style="background-image: url(<?php echo Data::getValue($images[6], 'imagepath') ?>)">
+	</a>
+	<a class="gallery_preview clicktoedit image eight index_7" href="<?php echo Data::getValue( $viewoptions, 'pre_slug' ) ?><?php echo Data::getValue( $images[7], 'slug' ) ?>" style="background-image: url(<?php echo Data::getValue($images[7], 'imagepath') ?>)">
+	</a>
+	<a class="gallery_preview clicktoedit image nine index_8" href="<?php echo Data::getValue( $viewoptions, 'pre_slug' ) ?><?php echo Data::getValue( $images[8], 'slug' ) ?>" style="background-image: url(<?php echo Data::getValue($images[8], 'imagepath') ?>)">
+	</a>
+</div>
+
+<script>
+
+		$('.gallery_preview.clicktoedit').click(function(e){
+			<?php
+				ReusableClasses::setUpEditingForSection( $viewdict, $viewoptions, $identifier );
+			?>
+		})
+
+
+</script>
