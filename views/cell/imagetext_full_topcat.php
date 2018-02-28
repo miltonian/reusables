91b5e8fc4be7834dc64815f451a4a48d5d2e36d5
Alex <miltonian3@gmail.com>
2018-02-27 23:18:17 -0500

    added template images. added cell: imagetext_full_topcat. improved design of two cells

---
+++ views/cell/imagetext_full_topcat.php
@@ -0,0 +1,49 @@
+<?php 
+
+	namespace Reusables;
+
+	$viewdict = Data::convertKeysInTable( $identifier, $viewdict );
+	
+	extract( Cell::prepareCell( $identifier ) );
+
+?>
+
+<div class="viewtype_cell imagetext_full_topcat main <?php echo $identifier ?> index_<?php echo $cellindex ?> <?php if($mediatype=="youtube" || $mediatype=="podcast"){ echo $mediatype; } ?> index_<?php echo $cellindex ?>" >
+	<div class="imagetext_full_topcat container">
+		<div style="display: inline-block; width: 100%;">
+			<div>
+				<a href="<?php echo $linkpath ?>">
+					<div class="imagetext_full_topcat picture" style="<?php echo 'background-image: url('.Data::getValue( $viewdict, 'imagepath', $table_identifier ).');'; ?>; <?php if( $mediatype == "video" ){ echo 'padding-bottom: 0;';  } ?>)">
+						<?php if($mediatype == "video"){ ?>
+							<video width="100%" height="auto" autoplay loop>
+							  <source src="<?php echo Data::getValue( $viewdict, 'featured_imagepath', $table_identifier ) ?>" type="video/mp4">
+							  <source src="<?php echo Data::getValue( $viewdict, 'featured_imagepath', $table_identifier ) ?>" type="video/ogg">
+							Your browser does not support the video tag.
+							</video>
+						<?php } ?>
+					</div>
+				</a>
+				<div class="imagetext_full_topcat words">
+					<div class="imagetext_full_topcat text-container">
+						<label class="imagetext_full_topcat category"><?php echo Data::getValue( $viewdict, 'category', $table_identifier ) ?></label>
+						<br>
+						<a href="<?php echo $linkpath ?>">
+							<label class="imagetext_full_topcat title" style=""><?php echo Data::getValue( $viewdict, 'title', $table_identifier ); ?></label>
+
+						</a>
+						<br>
+						<label class="imagetext_full_topcat grey-label"><?php echo $description ?></label>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+<script>
+
+	<?php
+		ReusableClasses::addEditingToCell( $identifier, $fullviewdict, $celltype );
+	?>;
+	
+</script>
