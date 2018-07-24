4add654079d81a86951bca18b3f8922e94acdf6b
Alex <miltonian3@gmail.com>
2018-07-24 01:42:41 -0400

    fixed bug in Views

--- classes/Views.php
+++ classes/Views.php
@@ -691,6 +691,8 @@ return;
 		$viewdict = Data::retrieveDataWithID( $identifier );
 		$viewoptions = Data::retrieveOptionsWithID( $identifier );
 
+		$tablename = Data::getDefaultTableNameWithData( $viewdict );
+
 		if( isset( $viewdict['value'] ) ) {
 
 			unset($viewdict['value']['data_id']);
@@ -729,6 +731,36 @@ return;
 				$fullviewdict = $dict;
 			}
 
+			$description = Data::getValue( $value, "html_text", $identifier );
+			if( $description == "" ) {
+				$description = Data::getValue( $value, "description", $identifier );
+			}
+			$preview = Data::getValue( $viewoptions, "preview", $identifier );
+			$description_limit = Data::getValue( $viewoptions, "description_limit", $identifier );
+
+			if( $preview == "true" || $preview ) {
+				$description = trim(preg_replace('/ +/', ' ', preg_replace('/[^A-Za-z0-9 ]/', ' ', urldecode(html_entity_decode(strip_tags($description))))));
+				if($description_limit ==  ""){
+					$description_limit = 300;
+				} else {
+					$description_limit = intval($description_limit);
+				}
+				if( $description != "" ) {
+					$description = Data::substrwords($description, $description_limit);
+				}
+			} else {
+				if($description_limit != ""){
+					if( $description != "" ) {
+						$description = Data::substrwords($description, intval($description_limit));
+					}
+				}
+			}
+
+if( $description != "" ) {
+	$dict['description'] = $description;
+	$dict['html_text'] = $description;
+}
+
 			array_push( $viewvalues, $dict );
 		}
 		if( !isset( $linkpath ) ) {
@@ -738,28 +770,9 @@ return;
 		$text_color = Data::getValue( $viewoptions, "text_color" );
 		$background_color = Data::getValue( $viewoptions, "background_color" );
 
-		$description = Data::getValue( $value, "html_text", $identifier );
-		if( $description == "" ) {
-			$description = Data::getValue( $value, "description", $identifier );
-		}
-		$preview = Data::getValue( $viewoptions, "preview", $identifier );
-		$description_limit = Data::getValue( $viewoptions, "description_limit", $identifier );
 
-		if( $preview == "true" || $preview ) {
-			$description = trim(preg_replace('/ +/', ' ', preg_replace('/[^A-Za-z0-9 ]/', ' ', urldecode(html_entity_decode(strip_tags($description))))));
-			if($description_limit ==  ""){
-				$description_limit = 300;
-			} else {
-				$description_limit = intval($description_limit);
-			}
-			$description = Data::substrwords($description, $description_limit);
-		} else {
-			if($description_limit != ""){
-				$description = Data::substrwords($description, intval($description_limit));
-			}
-		}
 
-		return ["viewvalues" => $viewvalues, "linkpath"=>$linkpath, "data_id"=>$identifier, "text_color"=>$text_color, "background_color"=>$background_color, "description"=>$description];
+		return ["viewvalues" => $viewvalues, "linkpath"=>$linkpath, "data_id"=>$identifier, "text_color"=>$text_color, "background_color"=>$background_color];
 	}
 
 	public static function setContainerClass($file, $identifier)
