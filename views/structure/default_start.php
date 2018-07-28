5fd99646e33042de7b8d179c3309f94c26cdb88b
Alex <miltonian3@gmail.com>
2018-07-28 17:41:23 -0400

    added margin and padding options to structure default_start

--- views/structure/default_start.php
+++ views/structure/default_start.php
@@ -2,6 +2,8 @@
 
 namespace Reusables;
 
+extract( Views::setUp( $identifier ) );
+
 $width = Data::getValue( $viewoptions, "width" );
 if( $width == "" ) {
   $width = "100%";
@@ -10,7 +12,14 @@ if( $width == "" ) {
 ?>
 
 <style>
-  .default.main.<?php echo $identifier ?> { display: inline-block; position: relative; margin: 0; padding: 0; float: left; width: <?php echo $width ?>; }
+  .default.main.<?php echo $identifier ?> { display: inline-block; position: relative; margin: 0; padding: 0; float: left; }
+  @media(min-width: 0px) {
+    .default.main.<?php echo $identifier ?> { padding: 0 !important; margin: 0 !important; width: 100%; }
+  }
+
+  @media(min-width: 768px) {
+    .default.main.<?php echo $identifier ?> { padding: <?php echo $padding ?> !important; margin: <?php echo $margin ?> !important; width: calc(<?php echo $width ?> - <?php echo $padding_width ?> - <?php echo $margin_width ?>); }
+  }
 </style>
 
 <div class="viewtype_structure <?php echo $identifier ?> default main">
